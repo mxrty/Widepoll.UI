cceda3008a5148a393794de724d7ec71c834a3a3
Marty Šreideris <Martynas67@gmail.com>
2022-07-22 22:57:45 +0100

    Header done

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,9 @@
-import Header from "./Header";
+import Header from "./Header.tsx";
 
 const Layout = ({ children }) => {
   return (
     <>
-      <Header />
+      <Header user={{ name: "mxrty" }} />
       <div className="grid md:grid-cols-5">
         <main className="md:col-start-2 md:col-end-5">{children}</main>
       </div>
